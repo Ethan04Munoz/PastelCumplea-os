c360aa56212c6f7ccc030b23060585ae0cd6f71c
Ethan <ethanmunozlopez04@gmail.com>
2024-11-20 11:18:22 -0600

    Funcion para apagar las velas

--- script.js
+++ script.js
@@ -13,5 +13,45 @@ function updateCandles() {
     }
 }
 
+function blowCandles() {
+    const style = document.createElement('style');
+    document.head.appendChild(style);
+
+    //1: Add the animation of stagger and strecht the flame
+    style.sheet.insertRule(`
+        .candle:before {
+            width: 6px;
+            height: 12px;
+            top: -14px;
+            left: 50%;
+            transform: translateX(-50%);
+            border-radius: 50%;
+            animation: flicker 0.3s infinite alternate;
+        }
+    `, 0);
+
+    // Animation of stagger and strecht
+    style.sheet.insertRule(`
+        @keyframes flicker {
+            0% { transform: translateX(-50%) scale(1, 1); }
+            50% { transform: translateX(-48%) scale(1.2, 1.3); }
+            100% { transform: translateX(-52%) scale(0.8, 0.5); }
+        }
+    `, 1);
+
+    //2: Turn off the candle
+    setTimeout(() => {
+        style.sheet.insertRule(`
+            .candle:before {
+                width: 0;
+                height: 0;
+                background-color: transparent;
+                animation: none;
+            }
+        `, 2);
+    }, 1200); 
+}
+
+
 // Initialize with default candle count
 updateCandles();
